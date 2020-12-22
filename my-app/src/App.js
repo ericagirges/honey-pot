2b63d8563b9b5fdd05ac6383a8543bfd25a83f8f
Erica Girges <ericafiorenza@gmail.com>
2020-12-22 15:42:55 -0800

    logo up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>HoneyPot</h1>
+        <h1>
+          <img src="./honeypot-logo.png" alt="honeypot logo in yellow and brown. Letter O is replaced with an icon of a beehive"/>
+        </h1>
       </header>
     </div>
   );
---
+++ my-app/src/data/dummyData.js
@@ -0,0 +1,96 @@
+const accessories = [
+    {
+        id: 101,
+        name: "Loungefly Disney The Three Caballeros Figural Mini Backpack",
+        productImg: "./3-caballeros-loungefly.png",
+    },
+    {
+        id: 102,
+        name: "Loungefly Disney 101 Dalmatians Cookie Mini Backpack",
+        productImg: "./dalmations-loungelfy.png"
+    },
+    {
+        id: 103,
+        name: "Nintendo Animal Crossing Fruits & Leaves Scrunchy Set",
+        productImg: "./animal-crossing.png"
+    },
+    {
+        id: 104,
+        name: "The Office Schrute Farms Cap",
+        productImg: "./shrute-farm.png"
+    },
+    {
+        id: 105,
+        name: "Taco Bell Logo Cap",
+        productImg: ""
+    }
+
+];
+
+const home = [
+    {
+        id: 106,
+        name: "Our Universe Studio Ghibli Kiki's Delivery Service Flying Throw"
+    },
+    {
+        id: 107,
+        name: "Studio Ghibli Spirited Away Haku Ramen Bowl with Chopsticks"
+    },
+    {
+        id: 108,
+        name: "SpongeBob SquarePants 2-Slice Toaster"
+    },
+    {
+        id: 109,
+        name: "Winking Boba Beverage Tumbler"
+    }
+
+];
+
+const collectibles = [
+    {
+        id: 110,
+        name: "Loungefly Disney Mulan Blind Box Enamel Pin"
+    },
+    {
+        id: 111,
+        name: "Funko Pop! Pin Dragon Ball Z Blind Box Enamel Pin"
+    },
+    {
+        id: 112,
+        name: "Funko Pop! WWE Mr. T Vinyl Figure"
+    },
+    {
+        id: 113,
+        name: "Funko Pop! Ad Icons Original Kool-Aid Packet Vinyl Figure"
+    }
+
+];
+
+const beauty = [
+    {
+        id: 114,
+        name: "Loungefly Beetlejuice Sandworm Makeup Brush Set & Holder"
+    },
+    {
+        id: 115,
+        name: "General Mills Breakfast Pack Lip Balm"
+    },
+    {
+        id: 116,
+        name: "Friend Central Perk Coffee Lip Scrub"
+    },
+    {
+        id: 117,
+        name: "Disney Princess Jasmine Whole New World Face Mask"
+    }
+
+];
+
+export {
+    accessories,
+    home,
+    collectibles,
+    beauty
+}
+
